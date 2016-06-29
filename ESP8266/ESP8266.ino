3eca68eb4bc79186fcf08173f84c0af3eca3c135
Marius-S <smigelskas.marius@gmail.com>
2016-06-30 00:38:19 +0300

    Added login into network functionality
    Powered by - https://github.com/tzapu/WiFiManager

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -1,100 +1,17 @@
-//IP http://192.168.4.1/
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 
-#include <ESP8266WiFi.h>
-const char WiFiAPPSK[] = "project2016";
-const int LED_PIN = 5; 
-const int ANALOG_PIN = A0; 
-const int DIGITAL_PIN = 12; 
-WiFiServer server(80);
+//#include <DNSServer.h>
+//#include <ESP8266WebServer.h>
+#include "libraries/WiFiManager/WiFiManager.h"         //https://github.com/tzapu/WiFiManager
 
-void initHardware()
-{
-  Serial.begin(115200);
-  pinMode(DIGITAL_PIN, INPUT_PULLUP);
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW);
+
+void setup() {
+    Serial.begin(115200);
+    WiFiManager wifiManager;
+    wifiManager.autoConnect("MS-Project", "project2016");
+    Serial.println("Connected");
 }
-void setupWiFi()
-{
-  WiFi.mode(WIFI_AP);
 
-  uint8_t mac[WL_MAC_ADDR_LENGTH];
-  WiFi.softAPmacAddress(mac);
-  String macID = String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) +
-                 String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
-  macID.toUpperCase();
-  String AP_NameString = "Project Arduino";
-
-  char AP_NameChar[AP_NameString.length() + 1];
-  memset(AP_NameChar, 0, AP_NameString.length() + 1);
-
-  for (int i=0; i<AP_NameString.length(); i++)
-    AP_NameChar[i] = AP_NameString.charAt(i);
-
-  WiFi.softAP(AP_NameChar, WiFiAPPSK);
-}
-void setup() { 
-  initHardware();
-  setupWiFi();
-  server.begin();
-}
-void loop(){
-   
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
-  String req = client.readStringUntil('\r');
-  Serial.println(req);
-  client.flush();
-
-  // Match the request
-  int val = -1; 
-                
-  if (req.indexOf("/led/0") != -1)
-    val = 0; // Will write LED low
-  else if (req.indexOf("/led/1") != -1)
-    val = 1; // Will write LED high
-  else if (req.indexOf("/read") != -1)
-    val = -2; // Will print pin reads
- 
-  if (val >= 0)
-    digitalWrite(LED_PIN, val);
-
-  client.flush();
-
-  String s = "HTTP/1.1 200 OK\r\n";
-  s += "Content-Type: text/html\r\n\r\n";
-  s += "<!DOCTYPE HTML>\r\n<html>\r\n";
-  // If we're setting the LED, print out a message saying we did
-  if (val >= 0)
-  {
-    s += "LED is now ";
-    s += (val)?"on":"off";
-  }
-  else if (val == -2)
-  { // If we're reading pins, print out those values:
-    s += "Analog Pin = ";
-    s += String(analogRead(ANALOG_PIN));
-    s += "<br>"; // Go to the next line.
-    s += "Digital Pin 12 = ";
-    s += String(digitalRead(DIGITAL_PIN));
-    s += "<br>";
-     s += "Uzklausa = ";
+void loop() {
     
-  }
-  else
-  {
-    s += "Invalid Request.<br> Try /led/1, /led/0, or /read.";
-  }
-  s += "</html>\n";
-
-  // Send the response to the client
-  client.print(s);
-  delay(1);
-  Serial.println("Client disonnected");
-
 }
-
-
-
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Arduino-and-ESP8266-project
 Arduino project is designed to create an autonomous robot and send data via ESP8266
+
+# Libraries
+__WiFiManager__
+https://github.com/tzapu/WiFiManager
